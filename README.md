b67aba53ca7dfd5089e5cd0a0b675e6efff77ae5
stefano.pirra <stefano.pirra@bitia.it>
2017-06-12 17:04:23 +0200

    added property deep search, fixed mess

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# node-red-filter with else support
-Original work by pste (https://github.com/pste/node-red-filter)
-A node that filters messages using the given property name and regular expression. When the regexp matches the whole message will be returned, otherwise it will be sent to the second output.</p>
-Example: \.(json|xml)$ matches all .json and .xml filenames 
+# node-red-filter
+A node that filters messages using the given property name and regular expression.
+The property name is deep-searched, so you can use something like "payload.my.prop" to test over "prop".
+When the regexp matches the whole message will be returned, otherwise the whole message will be dropped.
+Example: \.(json|xml)$ matches all .json and .xml filenames
+
--- filter.js
+++ filter.js
@@ -6,19 +6,38 @@ module.exports = function(RED) {
       this.ignorecase = config.ignorecase;
       
       var node = this;
+      var deepProperties = node.property.split("."); // payload.my.stuff =>  ["payload", "my", "stuff"] => { payload: { my: { stuff: ...
       var options = (node.ignorecase)?"i":"";
       var rx = null;
       try {
         rx  = new RegExp(node.filter, options);
       }
       catch (exception) {
+        //node.error(exception);
       }
       
       this.on('input', function(msg) {
-        if (rx!=null && msg.hasOwnProperty(node.property) && rx.test(msg[node.property]))
-          node.send([msg]);
-        else node.send([ null, msg ]);
-      });
+        // seek the property into the message
+        var prop = msg;
+        //node.log(">>>" + deepProperties);
+        for (var i=0; i<deepProperties.length; i++) {
+          //node.log(">>>" + deepProperties[i] + " => " + JSON.stringify(prop));
+          if (prop.hasOwnProperty(deepProperties[i])) {
+            prop = prop[deepProperties[i]];
+          }
+          else {
+            node.log("node-red-filter: message dropped (property '" + node.property + "' not found)");
+            prop = null;
+          }
+        }
+        //node.log(">>> testing " + rx + " over " + JSON.stringify(prop));
+
+        // here prop is the needed property value; now we need to check over the regexp
+        if (rx!=null && rx.test(prop))
+          node.send(msg)
+        else
+          node.log("node-red-filter: message dropped (not matching the '" + node.filter + "' regular expression)");
+      })
     }
     RED.nodes.registerType("filter", filterMsg);
 }
